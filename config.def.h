fc3ca8784a46fa241d5580f79697e0c1c1af9c1f
trvv <trvv@pm.me>
2019-05-04 01:06:24 -0400

    added libc version checks to example config

--- config.def.h
+++ config.def.h
@@ -3,6 +3,8 @@ Functions you can use to get information:
 
     hostname	system hostname
     username	user's name
+    libc	systems c library
+    libcversion version of system libc
     userinfo    user GECOS field
     osname	operating system name
     osversion	operating system version
@@ -12,5 +14,6 @@ Functions you can use to get information:
 
 item(BOLD BLUE "%s" RESET "@" BOLD BLUE "%s", username, hostname)
 item(BOLD GREEN "NM" BOLD BLACK "%12s", userinfo)
+item(BOLD GREEN "LC" BOLD BLACK "%12s", libc)
 item(BOLD GREEN "OS" BOLD BLACK "%12s", osname)
 item(BOLD GREEN "SH" BOLD BLACK "%12s", shellname)
